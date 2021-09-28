1df8e90a160306ce586c4c551d733957c2ea75c5
alemesa <notalemesa@gmail.com>
2021-09-28 16:37:30 -0400

    chore: add gsap custom ease example

--- src/lessons/withEase.js
+++ src/lessons/withEase.js
@@ -3,15 +3,23 @@ import { gsap } from "gsap";
 import Bezier from "bezier-easing";
 
 function WithEase() {
-    const Box6 = useRef(null);
+    const Box61 = useRef(null);
+    const Box62 = useRef(null);
 
     useEffect(() => {
-        const sampleBezier = new Bezier(0, 0, 1, 0.5);
+        const sampleBezier = new Bezier(0.17, 0.0, 0.28, 1.0);
+        const sampleBezier2 = gsap.parseEase("0.17, 0.0, 0.28, 1.0");
 
         gsap.fromTo(
-            Box6.current,
+            Box61.current,
             { duration: 1, x: 10 },
             { x: 300, delay: 1, ease: sampleBezier }
+        );
+
+        gsap.fromTo(
+            Box62.current,
+            { duration: 1, x: 10 },
+            { x: 300, delay: 1, ease: sampleBezier2 }
         );
     }, []);
 
@@ -19,8 +27,11 @@ function WithEase() {
         <React.Fragment>
             <div className="Parent">
                 <h2>with Easing</h2>
-                <div className="Box" ref={Box6}>
-                    6
+                <div className="Box" ref={Box61}>
+                    6.1
+                </div>
+                <div className="Box" ref={Box62}>
+                    6.2
                 </div>
             </div>
         </React.Fragment>
